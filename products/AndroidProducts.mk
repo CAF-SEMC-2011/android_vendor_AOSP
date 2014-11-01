4e11255d327135d0c73a5d8d14fbc6f3508243a1
Vynikal <Vynikal@seznam.cz>
2014-11-01 15:38:03 +0100

    Fix lunch combo

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,10 +1,10 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/haida.mk  \
-    $(LOCAL_DIR)/coconut.mk \
-    $(LOCAL_DIR)/mango.mk \
-    $(LOCAL_DIR)/smultron.mk \
-    $(LOCAL_DIR)/satsuma.mk \
-    $(LOCAL_DIR)/urushi.mk \
-    $(LOCAL_DIR)/anzu.mk \
-    $(LOCAL_DIR)/iyokan.mk \
-    $(LOCAL_DIR)/hallon.mk
+    $(LOCAL_DIR)/AOSP_haida.mk  \
+    $(LOCAL_DIR)/AOSP_coconut.mk \
+    $(LOCAL_DIR)/AOSP_mango.mk \
+    $(LOCAL_DIR)/AOSP_smultron.mk \
+    $(LOCAL_DIR)/AOSP_satsuma.mk \
+    $(LOCAL_DIR)/AOSP_urushi.mk \
+    $(LOCAL_DIR)/AOSP_anzu.mk \
+    $(LOCAL_DIR)/AOSP_iyokan.mk \
+    $(LOCAL_DIR)/AOSP_hallon.mk
